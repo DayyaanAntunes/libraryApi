1ffb7d8676c5e4dc727ba9f47f83f80baa557587
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2025-03-03 06:25:27 +0200

    feat(book): add BookMapper for entity-DTO transformations

---
+++ src/main/java/mz/gov/bau/libraryApi/book/domain/mapper/BookMapper.java
@@ -0,0 +1,24 @@
+package mz.gov.bau.libraryApi.book.domain.mapper;
+
+import mz.gov.bau.libraryApi.book.domain.command.BookCommand;
+import mz.gov.bau.libraryApi.book.domain.model.Book;
+import mz.gov.bau.libraryApi.book.presentation.BookJson;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface BookMapper {
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    Book toModel(BookCommand bookCommand);
+    BookJson toJson(Book book);
+    void toModel(BookCommand bookCommand, @MappingTarget Book book);
+    default List<BookJson> toJson(List<Book> books) {
+        return books.stream().map(this::toJson).collect(Collectors.toList());
+    }
+}
