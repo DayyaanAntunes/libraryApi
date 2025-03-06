7eccc6b48b54be33aa4067a5c534f1bf7d667c22
Dayyaan_Antunes <dantunes@technoplus.co.mz>
2025-03-06 22:37:14 +0200

    feat(exception): add basePackage to restControllerAdvice annotation and log Internal Server Error at command line

--- src/main/java/mz/gov/bau/libraryApi/config/exception/ControllerExceptionHandler.java
+++ src/main/java/mz/gov/bau/libraryApi/config/exception/ControllerExceptionHandler.java
@@ -1,23 +1,30 @@
 package mz.gov.bau.libraryApi.config.exception;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "mz.gov.bau")
+@Slf4j
 public class ControllerExceptionHandler {
+
+    @ResponseBody
     @ExceptionHandler(ResponseException.class)
     public ResponseEntity<ErrorResponse> handleResponseException(ResponseException exception) {
+        log.error("{} SERVER ERROR {} - {}", "\u001B[31m", "\u001B[0m", exception.getMessage());
         ErrorResponse errorResponse = new ErrorResponse(exception.getMessage(), exception.getTimestamp());
         return ResponseEntity.status(exception.getStatusCode()).body(errorResponse);
     }
 
+    @ResponseBody
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException() {
         ErrorResponse errorResponse = new ErrorResponse();
@@ -26,14 +33,7 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
-        ErrorResponse errorResponse = new ErrorResponse();
-        errorResponse.setMessage(exception.getMessage());
-        errorResponse.setTimestamp(LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    }
-
+    @ResponseBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         ErrorResponse errorResponse = new ErrorResponse();
--- src/main/java/mz/gov/bau/libraryApi/config/exception/ErrorResponse.java
+++ src/main/java/mz/gov/bau/libraryApi/config/exception/ErrorResponse.java
@@ -1,15 +1,17 @@
 package mz.gov.bau.libraryApi.config.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @Setter
+
 public class ErrorResponse {
     private String message;
     private LocalDateTime timestamp;
